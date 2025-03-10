2399fe3aee7e386ee5babbfcf1cc3f9dcf0b8007
Sayin Gonzalez <sayingonzalez@gmail.com>
2025-03-10 20:51:05 -0300

    Agregado: config. del Servidor y DB

---
+++ src/app.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import { connectDB } from './config/dbConfig.mjs';
+import superHeroRoutes from './routes/superHeroRoutes.mjs';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middleware para parsear JSON
+app.use(express.json());
+
+// Conexión a MongoDB
+connectDB();
+
+//configuración de rutas
+app.use('/api', superHeroRoutes);
+
+//Manejo de errores para rutas no encontradas
+app.use((req, res) => {
+    res.status(404).send({ mensaje: 'Ruta no encontrada' });
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor escuchado en el puerto ${PORT}`);
+});
